556afd3d4b82be1b8a431337c21e4551615e3b1d
Qutadah95 <qutadaalmomani@yahoo.com>
2021-07-05 15:27:28 +0300

    about me file

---
+++ js/app.js
@@ -0,0 +1,73 @@
+
+'use strict';
+
+let Name=prompt('what is your name ?');
+alert('Hello '+Name);
+
+alert('let us play a gussing game ');
+let answer=prompt('is my name Qutadah Almomani ?');
+
+ answer=answer.toLowerCase();
+ 
+
+ console.log(answer);
+if (answer==='y'||answer==='yah'||answer==='yes') {
+    alert('good guessing');
+    
+} else {
+    alert('bad guessing');
+}
+
+
+answer=prompt('do i create this website as assessment for lab02 ?');
+
+ answer=answer.toLowerCase();
+
+ console.log(answer);
+if (answer==='y'||answer==='yah'||answer==='yes') {
+    alert('good guessing');
+    
+} else {
+    alert('bad guessing');
+}
+
+
+answer=prompt('do i learn programming in this course ?');
+
+ answer=answer.toLowerCase();
+
+ console.log(answer);
+if (answer==='y'||answer==='yah'||answer==='yes') {
+    alert('good guessing');
+    
+} else {
+    alert('bad guessing');
+}
+
+
+answer=prompt(' i am 25 years old ,is that right ? ');
+
+ answer=answer.toLowerCase();
+
+ console.log(answer);
+if (answer==='y'||answer==='yah'||answer==='yes') {
+    alert('good guessing');
+    
+} else {
+    alert('bad guessing');
+}
+
+
+answer=prompt('did i study in one of a jordanian university ? ');
+
+ answer=answer.toLowerCase();
+
+ console.log(answer);
+if (answer==='y'||answer==='yah'||answer==='yes') {
+    alert('good guessing');
+    
+} else {
+    alert('bad guessing');
+}
+
+alert('thank you ' + Name + ' for play this game have a nice time in our website');
