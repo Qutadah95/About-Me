8651f906d8fc600fa5ca905a5bffe04fbaf7c84e
Qutadah95 <qutadaalmomani@yahoo.com>
2021-07-06 15:56:05 +0300

    add 2 question on gussing game

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # About-Me
 
-LAB - Number : Lab: 02
+LAB - Number : Lab- 02 / lab -03
 Project Name : About-Me
-I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+i just create a guessing game from 5 question and i used if condition
+ i add 2 question in guessing game and i used for loop
 
 Author: Qutadah Almomani
+
+
 Links and Resources
-submission PR
-Any Links you used as reference
-Reflections and Comments
-Consider including the answers to your daily journal and submission questions here
-This is also a good place to reflect on the tools and resources used and learned
+
+[W3schools](https://www.w3schools.com/js/js_arrays.asp)
+
+
--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
-'use strict';
 
+'use strict';
+let sum=0;
 let Name=prompt('what is your name ?');
 alert('Hello '+Name);
 
@@ -12,23 +13,26 @@ let answer=prompt('is my name Qutadah Almomani ?');
 
  console.log(answer);
 if (answer==='y'||answer==='yah'||answer==='yes') {
-    alert('good guessing');
-    
+    alert('you are right');
+    sum++;
+    console.log(sum);
 } else {
-    alert('bad guessing');
+    alert('you are wrong');
 }
 
 
-answer=prompt('do i create this website as assessment for lab02 ?');
+answer=prompt('do i create this website as assessment for lab ?');
 
  answer=answer.toLowerCase();
 
  console.log(answer);
 if (answer==='y'||answer==='yah'||answer==='yes') {
-    alert('good guessing');
+    alert('you are right');
+    sum++;
+    console.log(sum);
     
 } else {
-    alert('bad guessing');
+    alert('you are wrong');
 }
 
 
@@ -38,10 +42,12 @@ answer=prompt('do i learn programming in this course ?');
 
  console.log(answer);
 if (answer==='y'||answer==='yah'||answer==='yes') {
-    alert('good guessing');
+    alert('you are right');
+    sum++;
+    console.log(sum);
     
 } else {
-    alert('bad guessing');
+    alert('you are wrong');
 }
 
 
@@ -51,10 +57,12 @@ answer=prompt(' i am 25 years old ,is that right ? ');
 
  console.log(answer);
 if (answer==='y'||answer==='yah'||answer==='yes') {
-    alert('good guessing');
-    
+    alert('you are right');
+    sum++;
+
+    console.log(sum);
 } else {
-    alert('bad guessing');
+    alert('you are wrong');
 }
 
 
@@ -64,10 +72,73 @@ answer=prompt('did i study in one of a jordanian university ? ');
 
  console.log(answer);
 if (answer==='y'||answer==='yah'||answer==='yes') {
-    alert('good guessing');
+    alert('you are right');
+    sum++;
+    console.log(sum);
     
 } else {
-    alert('bad guessing');
+    alert('you are wrong');
 }
 
-alert('thank you ' + Name + ' for play this game have a nice time in our website');
+
+
+let num=prompt('guess a number betwen 1 and 50 ');
+
+
+for (let i = 1; i < 4; i++) {
+    
+    if(num===22)
+    {
+        sum++;
+        console.log(sum);
+         alert('you are right');
+        break;
+       
+    
+ 
+    }
+    else if(num<22) {
+        
+        num=prompt('you shoud try again your guessing is too short '+ 'this is your ' + i +' try');
+        
+    }
+    else if(num>22){
+        num=prompt('you shoud try again your guessing is too high '+ 'this is your ' + i +' try');
+    }
+  }
+
+ 
+alert('the corrict answer is 22');
+console.log(num);
+let cars = ["mitsubishi", "Volvo", "BMW","mercedes","ford"];
+let ans =prompt('what is my favorite car brand ');
+
+
+ans=ans.toLowerCase();
+console.log(ans);
+
+let q = 0 ;
+for (let i = 0; i < 5; i++) {
+    q++;
+    if (ans==='mitsubishi',ans==='volvo',ans==='bmw',ans==='mercedes',ans==='ford') { 
+        
+        alert('you are right');
+        sum++;
+        console.log(sum);
+       break;
+        
+        
+   
+        
+    }
+     else {ans =prompt('wrong guess try again' );
+       
+    }
+     
+}
+if (q==5){
+    alert('the right answer is "mitsubishi", "Volvo", "BMW","mercedes","ford"') 
+}
+console.log(ans);
+
+alert('thank you ' + Name + ' for play this game your score is '+sum+' /7 have a nice time in our website');
